c75eaad24d97993ebd5ebccd8a36fe29510e5f2a
Umut <umut.clsc01@outlook.com>
2026-08-23 15:54:36 +0300

    place search result

---
+++ place-project/Domain/LocationDomain/Data/PlaceSearchResult.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace place_project.Domain.LocationDomain.Data
+{
+    public class PlaceSearchResult
+    {
+        [Key]
+        public int Id { get; set; }
+        public string PlaceId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+
+        public string CommentMsg { get; set; } = "";
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string PhoneNumber { get; set; }
+        public double OverallScore { get; set; }
+
+        public string NormalizePhoneNumber(string phoneNumber)
+        {
+            PhoneNumber = new string((phoneNumber ?? string.Empty).Where(char.IsDigit).ToArray());
+            return PhoneNumber;
+        }
+    }
+}
