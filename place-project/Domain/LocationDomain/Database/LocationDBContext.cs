5a8b6d9409de9e57175b18df3a909df6705fe166
Umut <umut.clsc01@outlook.com>
2026-08-23 15:58:55 +0300

    database configuration

---
+++ place-project/Domain/LocationDomain/Database/LocationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using place_project.Domain.LocationDomain.Data;
+
+namespace place_project.Domain.LocationDomain.Database
+{
+    public class LocationDBContext : DbContext
+    {
+        public LocationDBContext(DbContextOptions<LocationDBContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<PlaceSearchResult> Locations { get; set; }
+    }
+}
