ceb88d5a0ea61167861b63ff230745df6bf7af49
Umut <umut.clsc01@outlook.com>
2026-08-25 20:00:51 +0300

    result added

---
+++ place-project/Domain/LocationDomain/Enums/Result.cs
@@ -0,0 +1,17 @@
+﻿namespace place_project.Domain.LocationDomain.Enums
+{
+
+    public enum CheckResult
+    {
+        NotFound,
+        Found
+
+    }
+    public enum ScoreResult
+    {
+        Perfect = 100,
+        Partial = 80,
+        Inconsistent = 60,
+        None = 40
+    }
+}
