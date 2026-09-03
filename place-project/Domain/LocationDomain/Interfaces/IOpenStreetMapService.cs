0c505a1ac3f35f5ceb07cf402691f821cfa65b4c
Umut <umut.clsc01@outlook.com>
2026-09-03 20:42:59 +0300

    place service added

---
+++ place-project/Domain/LocationDomain/Interfaces/IOpenStreetMapService.cs
@@ -0,0 +1,9 @@
+﻿using place_project.Domain.LocationDomain.Data;
+
+namespace place_project.Domain.LocationDomain.Interfaces
+{
+    public interface IOpenStreetMapService
+    {
+        Task<PlaceSearchResult?> FindPlaceAsyncOSM(BusinessPlace place);
+    }
+}
