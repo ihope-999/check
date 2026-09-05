5841d9d009b0491e9b293b74ccf546cf91a83ef9
Umut <umut.clsc01@outlook.com>
2026-09-05 23:35:30 +0300

    place service added

---
+++ place-project/Domain/LocationDomain/Interfaces/IWikiLocationService.cs
@@ -0,0 +1,9 @@
+﻿using place_project.Domain.LocationDomain.Data;
+
+namespace place_project.Domain.LocationDomain.Interfaces
+{
+    public interface IWikiLocationService
+    {
+        Task<PlaceSearchResult?> FindPlaceAsync(BusinessPlace place);
+    }
+}
