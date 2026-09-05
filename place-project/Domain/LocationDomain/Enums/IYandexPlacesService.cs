02d05f171b41dd54290f8ef06c911995e7570cba
Umut <umut.clsc01@outlook.com>
2026-09-05 23:36:07 +0300

    place service added

---
+++ place-project/Domain/LocationDomain/Enums/IYandexPlacesService.cs
@@ -0,0 +1,9 @@
+﻿using place_project.Domain.LocationDomain.Data;
+
+namespace place_project.Domain.LocationDomain.Enums
+{
+    public interface IYandexPlacesService
+    {
+        public Task<PlaceSearchResultYandex?> FindPlaceAsync(BusinessPlace place);
+    }
+}
